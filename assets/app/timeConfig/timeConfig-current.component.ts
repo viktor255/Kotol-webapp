6397cd94f65b4b684b30974d6034558e5feeced7
viktor255 <viktor.lehotsky@gmail.com>
2018-04-10 16:59:12 +0200

    rozdelene chceck local a DB

--- assets/app/timeConfig/timeConfig-current.component.ts
+++ assets/app/timeConfig/timeConfig-current.component.ts
@@ -26,39 +26,48 @@ import { TimeConfigService } from "./timeConfig.service";
 export class TimeConfigCurrentComponent implements OnInit, OnDestroy {
     @Input() timeConfigCurrent: TimeConfig;
     @Input() desiredTemperature: number;
-
+    lastConfigTime;
     intervalId;
 
     constructor(private timeConfigService: TimeConfigService) {
     }
 
-    checkCurrentTemp() {
+    checkCurrentTempDB() {
         this.timeConfigService.getCurrentTimeConfig()
             .subscribe((timeConfig: TimeConfig) => {
+                this.lastConfigTime = Number(timeConfig.time);
                 this.timeConfigCurrent = timeConfig;
-                const currentTime = new Date().getTime();
-                const lastConfigTime = Number(timeConfig.time);
-                this.timeConfigCurrent.time = (new Date(lastConfigTime)).toString();
-                this.desiredTemperature = this.timeConfigService.desiredTemperature();
-                let box = document.getElementById("tempBox");
-                if (((currentTime - lastConfigTime) > 120000) || this.desiredTemperature != this.timeConfigCurrent.temperature)
-                    box.style.background = 'red';
-                else
-                    box.style.background = 'rgba(73, 145, 85, 0.74)';
+                this.timeConfigCurrent.time = (new Date(Number(timeConfig.time))).toString();
             });
+    }
+
+    checkCurrentTempLocal() {
+        if(this.timeConfigCurrent) {
+            const currentTime = new Date().getTime();
+            this.desiredTemperature = this.timeConfigService.desiredTemperature();
+            let box = document.getElementById("tempBox");
+            if (((currentTime - this.lastConfigTime) > 120000) || this.desiredTemperature != this.timeConfigCurrent.temperature)
+                box.style.background = 'red';
+            else
+                box.style.background = 'rgba(73, 145, 85, 0.74)';
+        }
+
     }
 
     ngOnInit() {
         setTimeout(() => {
-            this.checkCurrentTemp()
+            this.checkCurrentTempDB()
         }, 2000);
-        // this.checkCurrentTemp();
-        this.intervalId = setInterval(() => {
-            this.checkCurrentTemp()
+        // this.checkCurrentTempLocal();
+        setInterval(() => {
+            this.checkCurrentTempDB()
         }, 30000);
+        this.intervalId = setInterval(() => {
+            this.checkCurrentTempLocal()
+        }, 3000);
     }
 
-    ngOnDestroy(){
+    ngOnDestroy() {
         clearInterval(this.intervalId);
     }
 }
